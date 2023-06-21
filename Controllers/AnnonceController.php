d7e77718d4b2f3277cdc24693e515ff885f4959d
Abdou Samath DIOUF <samathabdou98@gmail.com>
2023-06-21 04:51:04 +0200

    Routeur fonctionne

---
+++ Controllers/AnnonceController.php
@@ -0,0 +1,8 @@
+<?php 
+namespace App\Controllers;
+class AnnonceController extends Controller
+{
+    public function index() {
+        echo "Ici c'est la liste des annonce";
+    }
+}
--- Public/index.php
+++ Public/index.php
@@ -1,18 +1,21 @@
 <?php
 // On definit une constante qui contient le dossier racine de notre projet
 define('ROOT', dirname(__DIR__));
-var_dump(ROOT);
 
 use App\vendor\autoload;
 use App\src\Router;
 
 // ON importe l'autoloader
 require_once ROOT. '/vendor/autoload.php';
+//autoload::register();
+// Importer le routeur
+require_once ROOT. '/src/Router.php';
+
 
 //On instancie Router
-$app=new Router();
+ $app=new Router();
 
-// One démarre l'application
-$app->start();
+// On démarre l'application
+$app->start(); 
 
 
